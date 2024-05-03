259a1c1563f04d17b67f74b2aec4be77faafeeea
prapti2408 <163879184+prapti2408@users.noreply.github.com>
2024-05-03 17:55:48 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,41 +1,95 @@
 
-import ollama
-import streamlit as st
+import streamlit as st 
+from streamlit_chat import message
+import tempfile
 
-st.title("Ollama Python Chatbot")
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import CTransformers
+from huggingface_hub import hf_hub_download
+from langchain.chains import ConversationalRetrievalChain
 
-# initialize history
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+DB_FAISS_PATH = 'vectorstore/db_faiss'
 
-# init models
-if "model" not in st.session_state:
-    st.session_state["model"] = ""
 
-models = [model["name"] for model in ollama.list()["models"]]
-st.session_state["model"] = st.selectbox("Choose your model", models)
+def load_llm():
 
-def model_res_generator():
-    stream = ollama.chat(
-        model=st.session_state["model"],
-        messages=st.session_state["messages"],
-        stream=True,
+    llm = CTransformers(
+        model = "llama-2-7b-chat.ggmlv3.q8_0.bin",
+        model_type="llama",
+        max_new_tokens = 512,
+        temperature = 0.7
     )
-    for chunk in stream:
-        yield chunk["message"]["content"]
+    return llm
 
-# Display chat messages from history on app rerun
-for message in st.session_state["messages"]:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+st.title("Chatbot")
 
-if prompt := st.chat_input("What is up?"):
-    # add latest message to history in format {role, content}
-    st.session_state["messages"].append({"role": "user", "content": prompt})
+uploaded_file = st.sidebar.file_uploader("Upload your Data", type="csv")
 
-    with st.chat_message("user"):
-        st.markdown(prompt)
+if uploaded_file :
+  
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(uploaded_file.getvalue())
+        tmp_file_path = tmp_file.name
 
-    with st.chat_message("assistant"):
-        message = st.write_stream(model_res_generator())
-        st.session_state["messages"].append({"role": "assistant", "content": message})
+    loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={
+                'delimiter': ','})
+    data = loader.load()
+
+    hf_hub_download(
+    repo_id="TheBloke/Llama-2-7B-Chat-GGML",
+    filename="llama-2-7b-chat.ggmlv3.q8_0.bin",
+    local_dir="./models"
+)
+   
+
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device': 'cpu'})
+
+    db = FAISS.from_documents(data, embeddings)
+    db.save_local(DB_FAISS_PATH)
+    llm = load_llm()
+    chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
+
+    def conversational_chat(query):
+        result = chain({"question": query, "chat_history": st.session_state['history']})
+        st.session_state['history'].append((query, result["answer"]))
+        return result["answer"]
+    
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
+
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hello ! Ask me anything about " + uploaded_file.name ]
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hey ! 👋"]
+        
+  
+    response_container = st.container()
+  
+    container = st.container()
+
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            
+            user_input = st.text_input("Query:", placeholder="Talk to your csv data here (:", key='input')
+            submit_button = st.form_submit_button(label='Send')
+            
+        if submit_button and user_input:
+            output = conversational_chat(user_input)
+            
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+  
+
+    if st.session_state['generated']:
+        with response_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["generated"][i], key=str(i))
+
+
+
+    
